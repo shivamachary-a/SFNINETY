c29411fd09e39ca033afc1ca20c0db8dd7c61c83
Shivam Acharya <shivam@shivams-mbp.home>
2021-06-13 01:06:34 +0100

    added alpha testing link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,15 @@
-import { Box, Flex } from "@chakra-ui/layout";
+import { Box, Flex, Spacer } from "@chakra-ui/layout";
+import { Link, Button } from "@chakra-ui/react"
 
 const NavBar = () => {
 	return (
 		<Box>
 			<Flex justifyContent="center" alignItems="center" p="0.5em" maxH="10vh">
 				<img alt="logo" style={{ height: "6vh", width: "auto" }} src="https://cdn.discordapp.com/attachments/692861972731002995/852270505528655912/output-onlinepngtools_1.png"></img>
+				<Spacer />
+				<Link href="https://app.sfninety.com">
+					<Button>Alpha tester? Get started here.</Button>
+				</Link>
 			</Flex>
 		</Box>
 	);
